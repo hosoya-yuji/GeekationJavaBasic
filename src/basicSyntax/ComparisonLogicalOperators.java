02462a1259971b382e6066e7da35866835e42274
hosoya-yuji <hosoya1993@yahoo.co.jp>
2024-05-31 21:57:42 +0900

    compari

--- src/basicSyntax/ComparisonLogicalOperators.java
+++ src/basicSyntax/ComparisonLogicalOperators.java
@@ -23,12 +23,6 @@ public class ComparisonLogicalOperators {
 		boolean hasPermission = false;
 		boolean notHasPermission = !hasPermission;
 		System.out.println(notHasPermission);
-		
-<<<<<<< HEAD
-		
-=======
-
->>>>>>> refs/heads/main
 	}
 
 }
