7f90a0dd852e360402a59c8143770fa5ac8a4257
hosoya-yuji <hosoya1993@yahoo.co.jp>
2024-06-02 03:43:59 +0900

    method-practice

---
+++ src/basicSyntax/MethodPractice.java
@@ -0,0 +1,52 @@
+package basicSyntax;
+
+public class MethodPractice {
+	
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		
+        int sum = sumNumbers(a, b);
+        numbersResult(sum);
+
+        int sub = subNumbers(a, b);
+        numbersResult(sub);
+
+        int multi = multiNumbers(a, b);
+        numbersResult(multi);
+
+        int divi = diviNumbers(a, b);
+        numbersResult(divi);
+	}
+
+	public static int sumNumbers(int a, int b) {
+		return  a + b;
+				
+	}
+	
+	public static int subNumbers(int a, int b) {
+		return a - b;
+
+	}
+	
+	public static int multiNumbers(int a, int b) {
+		return a * b;
+
+	}
+	
+	public static int diviNumbers(int a, int b) {
+		return a / b;
+		
+
+	}
+	
+	public static void numbersResult(int result) {
+		System.out.println("計算結果は" + result + ": です。 ");
+	}
+	
+
+		
+	
+}
+
+
