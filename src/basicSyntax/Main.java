4bfac68d4e9d4f9da95072009181eda6c4df1d0b
hosoya-yuji <hosoya1993@yahoo.co.jp>
2024-06-02 18:31:16 +0900

    class

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,14 @@
+package basicSyntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GeekIntroduction geek = new GeekIntroduction();
+        
+        // 挨拶メソッドの呼び出し
+        geek.greeting(30, 162.5);
+
+        // 特技メソッドの呼び出し
+        geek.specialSkill(1, 20);
+    }
+}
