0d2dda427dc5d8f3f16f4dd36da077c41303199c
hosoya-yuji <hosoya1993@yahoo.co.jp>
2024-06-17 17:28:02 +0900

    add-geek-introduction-class

---
+++ src/basicSyntax/Main.java
@@ -0,0 +1,14 @@
+package basicSyntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GeekIntroduction geek = new GeekIntroduction();
+        
+       
+        geek.greeting(30, 162.5);
+
+       
+        geek.specialSkill(1, 20);
+    }
+}
